e21d9453c6abcccbf5bb4b6630a07d920a44247c
Emma de Silva <erdesilva25@gmail.com>
2025-03-29 19:44:22 -0500

    [ed] adding fonts and fixing mobile menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
