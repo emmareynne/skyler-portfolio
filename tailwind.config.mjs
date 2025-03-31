178d2ada2cf9ea2ae55c56d4faf659f0e7fcdd77
Emma de Silva <erdesilva25@gmail.com>
2025-03-30 19:21:50 -0500

    [ed] fixing responsive design, adding background animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'sans': ['Frank Ruhl Libre', 'serif'],
+			'serif': ['Frank Ruhl Libre', 'serif'],
+			'display': ['Yuji Mai', 'Yuji Syuku', 'serif'],
+		'heading': ['Faculty Glyphic', 'sans-serif'],
+		},
 		extend: {
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-			},
-			fontFamily: {
-				'yuji': ['Yuji Mai', 'Yuji Syuku', 'serif'],
 			},
 		},
 	},
